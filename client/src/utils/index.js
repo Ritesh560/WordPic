6bf73c7f8757a9571df09430d0a5daf7fcc090c2
Ritesh Kumar <singlaritesh007@gmail.com>
2023-04-30 17:39:44 +0530

    fix: redirections

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -10,24 +10,6 @@ export function getRandomPrompt(prompt) {
   return randomPrompt
 }
 
-export async function downloadImage(_id, url) {
-  // FileSaver.saveAs(photo, `download-${_id}.jpg`);
-  const response = await fetch(url)
-  const blob = await response.blob()
-
-  // Create a URL for the blob
-  const blobUrl = URL.createObjectURL(blob)
-
-  // Create an anchor element with the blob URL
-  const anchor = document.createElement("a")
-  anchor.href = blobUrl
-
-  // Set the download attribute to force download
-  anchor.download = `${_id}.jpg`
-
-  // Simulate a click on the anchor element to initiate download
-  anchor.click()
-
-  // Clean up by revoking the blob URL
-  URL.revokeObjectURL(blobUrl)
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`)
 }
