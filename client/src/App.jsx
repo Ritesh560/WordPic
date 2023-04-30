0dc5cb3ba0fc717c2e6845374bc41362916b1655
Ritesh Kumar <singlaritesh007@gmail.com>
2023-04-30 12:08:36 +0530

    feat: color gradients

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import React from "react"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 
-import { logo } from './assets';
-import { Home, CreatePost } from './page';
+import { Home, CreatePost } from "./page"
+import { wordpic } from "./assets"
+import "./app.scss"
 
 const App = () => (
   <BrowserRouter>
-    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-      <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+    <header className="w-full flex justify-between items-center bg-[#b5d6e5] sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/" className="flex items-center gap-3">
+        <img src={wordpic} alt="logo" className="h-12 object-contain" />
+        <span className="font-inter font-bold text-lg">WordPic</span>
       </Link>
 
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+        Create
+      </Link>
     </header>
-    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+    <main className="sm:p-8 px-4 py-8 w-full min-h-[calc(100vh-73px)] wordPic_container">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </main>
   </BrowserRouter>
-);
+)
 
-export default App;
+export default App
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,6 @@
-import download from './download.png';
-import logo from './logo.svg';
-import preview from './preview.png';
+import download from "./download.png"
+import logo from "./logo.svg"
+import preview from "./preview.png"
+import wordpic from "./wordPic_logo.png"
 
-export {
-  download,
-  logo,
-  preview,
-};
+export { download, logo, preview, wordpic }
